e25d68630bc6abea903bbb309f03e8f6192ba50e
David-Rubio-Caballlero <d.rubio.2019@alumnos.urjc.es>
2023-04-13 21:25:05 +0200

    Clases base para el proyecto, a falta de procesos y patrones

---
+++ src/fase3mp/FabricaArmaduras.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fase3mp;
+
+/**
+ *
+ * @author d.rubio.2019
+ */
+public class FabricaArmaduras extends FabricaEquipo{
+    @Override
+    public Equipo crearEquipo(){
+        Armadura armadura = new Armadura();
+        return armadura;
+    }
+}
