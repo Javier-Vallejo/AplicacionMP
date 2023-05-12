008bf2abd556654e52a5871d3f5ccc8ad0ded473
Juan Alvarez <j.alvarezl.2019@alumnos.urjc.es>
2023-05-12 17:21:38 +0200

    Test's de Licantropo, y Vampiro. + Cambio en el codigo de Licantropo que usaba el daño total como valor para la defensa.

--- test/fase3mp/LicantropoTest.java
+++ test/fase3mp/LicantropoTest.java
@@ -35,6 +35,30 @@ public class LicantropoTest {
     @After
     public void tearDown() {
     }
+    
+    public Licantropo CrearLicantropoBase () {
+
+        Habilidad habilidad = new Habilidad("garrasToFUertes", 10, 10, 0);
+        Arma arma = new Arma("arma", "10", "10", "de2manos");
+        Arma[] armas = new Arma[1];
+        armas[0] = arma;
+        Armadura armadura = new Armadura("armadura", "10", "5");
+        Armadura[] armaduras = new Armadura[1];
+        armaduras[0] = armadura;
+        Esbirro esbirro = new Esbirro("esbirro", 10);
+        Esbirro[] esbirros = new Esbirro[1];
+        esbirros[0] = esbirro;
+        Debilidad debilidad = new Debilidad("debilidad", 0);
+        Debilidad[] debilidades = new Debilidad[1];
+        debilidades[0] = debilidad;
+        Fortaleza fortaleza = new Fortaleza("fortaleza", 0);
+        Fortaleza[] fortalezas = new Fortaleza[1];
+        fortalezas[0] = fortaleza;
+        Licantropo instance = new Licantropo("ElQueNoEsEdward", habilidad, armas, armas, armaduras, armadura, esbirros, 5, 5, debilidades, fortalezas); 
+        instance.setRabia(3);
+
+        return instance;
+    }
 
     /**
      * Test of getRabia method, of class Licantropo.
@@ -42,12 +66,12 @@ public class LicantropoTest {
     @Test
     public void testGetRabia() {
         System.out.println("getRabia");
-        Licantropo instance = null;
-        int expResult = 0;
+        Licantropo instance = CrearLicantropoBase();
+        int expResult = 2;
         int result = instance.getRabia();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        //Nuestra rabia base es 3, así que este test falla aposta
+        assertNotEquals(expResult, result);
+        assertEquals(3, result);
     }
 
     /**
@@ -57,15 +81,16 @@ public class LicantropoTest {
     public void testSetRabia() {
         System.out.println("setRabia");
         int rabia = 0;
-        Licantropo instance = null;
+        Licantropo instance = CrearLicantropoBase();
+        System.out.println("Rabia previa al cambio: " + instance.getRabia());
         instance.setRabia(rabia);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        System.out.println("Rabia tras el set: " + instance.getRabia());
     }
 
     /**
      * Test of rellenarPropiedadesEspecificas method, of class Licantropo.
-     */
+    *Se trata de una funcion en la que se setean los valores introducidos en teclado,
+    * por lo que no vemos necesidad de testearla teniendo las funciones previas
     @Test
     public void testRellenarPropiedadesEspecificas() {
         System.out.println("rellenarPropiedadesEspecificas");
@@ -73,7 +98,7 @@ public class LicantropoTest {
         instance.rellenarPropiedadesEspecificas();
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
-    }
+    } */
 
     /**
      * Test of clonar method, of class Licantropo.
@@ -81,12 +106,12 @@ public class LicantropoTest {
     @Test
     public void testClonar() {
         System.out.println("clonar");
-        Licantropo instance = null;
-        Personaje expResult = null;
+        Licantropo instance = CrearLicantropoBase();
+        Personaje expResult = CrearLicantropoBase();
         Personaje result = instance.clonar();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals(expResult.getNombre(), result.getNombre());
+        System.out.println("Nombre de la habilidad del Licantropo: " + instance.getNombre() + " = " + instance.getHabilidadPersonaje().getNombre());
+        assertNotEquals("GarrasTOFUERTOTAS", result.getHabilidadPersonaje().getNombre());
     }
 
     /**
@@ -95,12 +120,18 @@ public class LicantropoTest {
     @Test
     public void testCalculoDanio() {
         System.out.println("calculoDanio");
-        Licantropo instance = null;
-        int expResult = 0;
+        Licantropo instance = CrearLicantropoBase();
+        int expResult = 48;
         int result = instance.calculoDanio();
+        System.out.println("Danio activando el Don: " + result);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        instance.getHabilidadPersonaje().setLimitante(4);
+        result = instance.calculoDanio();
+        //El daño se reduce en 20 por no activar el don, pasa de 48 a 28
+        int exp2 = 28;
+        System.out.println("Danio NO activando el don: " + result);
+        assertNotEquals(expResult, result);
+        assertEquals(exp2, result);
     }
 
     /**
@@ -109,12 +140,10 @@ public class LicantropoTest {
     @Test
     public void testCalculoDefensa() {
         System.out.println("calculoDefensa");
-        Licantropo instance = null;
-        int expResult = 0;
+        Licantropo instance = CrearLicantropoBase();
+        int expResult = 30;
         int result = instance.calculoDefensa();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
     
 }
--- test/fase3mp/ManagerUsuariosTest.java
+++ test/fase3mp/ManagerUsuariosTest.java
@@ -120,8 +120,6 @@ public class ManagerUsuariosTest {
         assertEquals(false, result2);
         assertEquals(false, result3);
         assertEquals(false, result4);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
